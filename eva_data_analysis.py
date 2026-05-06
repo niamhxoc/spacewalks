65cf2772396c224028347d7193140ed82f262bd6
Niamh O'Callaghan <56365242+niamhxoc@users.noreply.github.com>
2026-05-06 12:51:50 +0100

    Changed directory structure and refactored code

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,5 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+import seaborn as sns
+
+def main(input_file, output_file, graph_file):
+    print("--START--")
+
+    # Read the data from JSON file
+    eva_data = read_json_to_dataframe(input_file)
+
+    # Convert and export data to CSV file
+    write_dataframe_to_csv(eva_data, output_file)
+
+    # Calculate the time spent in space as a function of time and create a plot of the data
+    plot_time_spent_in_space(eva_data,graph_file)
+
+    print("--END--")
 
 def read_json_to_dataframe(input_file):
     """
@@ -34,7 +50,7 @@ def write_dataframe_to_csv(df, output_file):
     # Write EVA data to csv file 
     df.to_csv(output_file, index=False, encoding='utf-8') 
 
-def plot_time_spent_in_space(df):
+def plot_time_spent_in_space(df,graph_file):
     """
     Calculate the cumulative time spent in space as a function of time. Then plot the data as time spent in space on the y axis, and the date on the x axis
 
@@ -45,32 +61,62 @@ def plot_time_spent_in_space(df):
  
     # Calculate cumulative time spent in space
     df.sort_values('date', inplace=True) # Sort EVA missions in df by date
-    df['duration_hours'] = df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60) # Extract each mission duration and convert to hours (float)
+    df = add_duration_hours(df)
     df['cumulative_time'] = df['duration_hours'].cumsum() # Cumulatively sum and write each mission duration through time
 
     # Create plot of cumulative time spent in space through time
-    plt.plot(eva_data['date'], eva_data['cumulative_time'], 'ko-')
+    sns.set_theme()
+    sns.lineplot(data=df, x="date", y="cumulative_time", marker='.')
+    #plt.plot(df['date'], df['cumulative_time'], 'ko-')
     plt.xlabel('Year')
     plt.ylabel('Total time spent in space to date (hours)')
-    plt.grid(alpha=0.2)
     plt.tight_layout()
     plt.savefig(graph_file)
     plt.show()
 
-# Data source: https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = open('./eva-data.json', 'r', encoding='ascii')
-output_file = open('./eva_data_analysis.csv', 'w', encoding='utf-8')
-graph_file = './cumulative_eva_figure.png'
+def text_to_duration(duration):
+    """
+    Convert a text format duration "HH:MM" to duration in hours
 
-print("--START--")
+    Args:
+        duration (str): The text format duration
 
-# Read the data from JSON file
-eva_data = read_json_to_dataframe(input_file)
+    Returns:
+        duration_hours (float): The duration in hours
+    """
+    hours, minutes = duration.split(":")
+    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    return duration_hours
 
-# Convert and export data to CSV file
-write_dataframe_to_csv(eva_data, output_file)
 
-# Calculate the time spent in space as a function of time and create a plot of the data
-plot_time_spent_in_space(eva_data)
+def add_duration_hours(df):
+    """
+    Add duration in hours (duration_hours) variable to the dataset
 
-print("--END--")
+    Args:
+        df (pd.DataFrame): The input dataframe.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of df with the new duration_hours variable added
+    """
+    df_copy = df.copy()
+    df_copy["duration_hours"] = df_copy["duration"].apply(
+        text_to_duration
+    )
+    return df_copy
+
+
+# Main code
+if __name__=="__main__":
+
+    if len(sys.argv) < 3:
+        input_file = open('data/eva-data.json', 'r', encoding='ascii')
+        output_file = open('results/eva_data_analysis.csv', 'w', encoding='utf-8')
+        print('Using default input and output filenames.')
+    else:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+        print('Using custom input and output filenames.')
+
+    graph_file = 'results/cumulative_eva_figure.png'
+    main(input_file,output_file,graph_file)
